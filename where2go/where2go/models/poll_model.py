dd0f55e05b75cd04f64a86728e801133782df0dc
frascalise <scalise92111@gmail.com>
2025-09-20 18:31:23 +0200

    small fixes

--- where2go/where2go/models/poll_model.py
+++ where2go/where2go/models/poll_model.py
@@ -11,13 +11,22 @@ class Categories(models.Model):
 
 '''
     Restaurant model representing a restaurant in the database.
+    TBA:
+    - Location
+    - Price range
+    - Cuisine type
+    - Reviews
 '''
 class Restaurants(models.Model):
     name = models.CharField(max_length=200)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
 
 
 '''
-    Contains the votes of users for different restaurants.
+    Contains the votes of users for different categories.
+    Each vote links a user to a category.
+    After each poll, votes are cleared.
+
 '''
 class Votes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
