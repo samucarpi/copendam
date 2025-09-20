ffbe4f546d05a56f12506d5044fe74bc5a90d31b
frascalise <scalise92111@gmail.com>
2025-09-20 18:27:04 +0200

    added poll_model.py

--- where2go/where2go/models/poll_model.py
+++ where2go/where2go/models/poll_model.py
@@ -2,11 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+'''
+    Categories model representing different categories of restaurants.
+'''
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+
+
+'''
+    Restaurant model representing a restaurant in the database.
+'''
 class Restaurants(models.Model):
     name = models.CharField(max_length=200)
 
 
+'''
+    Contains the votes of users for different restaurants.
+'''
 class Votes(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    restaurant = models.ForeignKey(Restaurants, on_delete=models.CASCADE)
+    category = models.ForeignKey(Restaurants, on_delete=models.CASCADE)
 
